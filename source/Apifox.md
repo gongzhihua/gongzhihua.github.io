20bb1b4079f5b13d15d09f3ef7c496d7c11c3502
gongzhihua <gongzhihua@qq.com>
2022-07-25 10:05:42 +0800

    feat: 修改测试

--- source/Apifox.md
+++ source/Apifox.md
@@ -4,3 +4,4 @@ Apifox
 ## Apifox介绍
 Apifox 是 API 文档、API 调试、API Mock、API 自动化测试一体化协作平台，定位 Postman + Swagger + Mock + JMeter。通过一套系统、一份数据，解决多个系统之间的数据同步问题。
 
+## APifox的使用
--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ source_suffix = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
